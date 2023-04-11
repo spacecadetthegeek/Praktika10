e6d8e0c4c6867ebacb1ddf5c754743127da1b22e
spacecadetthegeek <mrvovashka@gmail.com>
2023-04-11 03:30:24 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Calculate
+{
+    static void Main(string[] args)
+    {
+        int a = 0, b = 0, result = 0;
+
+        ISum operation = new Addition();
+
+        try
+        {
+            Reader(out a, out b);
+            result = operation.Sum(a, b);
+
+            Console.WriteLine($"Результат: {a} + {b} = {result}");
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Введено неверное значение!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: ", ex.Message);
+        }
+
+    }
+
+    static public void Reader(out int a, out int b)
+    {
+        Console.WriteLine("Введите a: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите b: ");
+        b = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+public interface ISum
+{
+    public int Sum(int a, int b);
+}
+
+class Addition : ISum
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CalculatorWithLogger
+{
+    public interface ICalculator
+    {
+        int Sum(int a, int b);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Event: " + message);
+            Console.ResetColor();
+        }
+
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: " + message);
+            Console.ResetColor();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator calculator = new Calculator();
+            ILogger logger = new Logger();
+
+            while (true)
+            {
+                Console.WriteLine("Введите 1 число:");
+                string input1 = Console.ReadLine();
+                int a;
+
+                Console.WriteLine("Введите 2 число:");
+                string input2 = Console.ReadLine();
+                int b;
+
+                try
+                {
+                    a = int.Parse(input1);
+                    b = int.Parse(input2);
+                }
+                catch (FormatException)
+                {
+                    logger.Error("Ошибка");
+                    continue;
+                }
+
+                int sum = calculator.Sum(a, b);
+                Console.WriteLine("Результат: " + sum);
+                logger.Event("Сумма " + a + " и " + b + " равна " + sum);
+            }
+        }
+    }
+}
